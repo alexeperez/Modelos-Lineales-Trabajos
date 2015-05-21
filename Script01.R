3132c4a4a558fb60559a5b8f0f31c141313e2352
lexperez90@hotmail.com <lexperez90@hotmail.com>
2015-05-21 18:07:42 -0500

    Trabajo 01

---
+++ Script01.R
@@ -0,0 +1,53 @@
+##-------------------------------------------------##
+##-----------      Modelos Linelaes     -----------##
+##-----------        Trabajo 01         -----------##
+##-- Nombre:
+
+
+# 2.1 Leer el archivo de datos data.txt, y analizar de que estructura de datos se trata.
+# Utilice la función read.table()
+
+# 2.2 Calcular el mínimo, la media, el máximo de la variable Edad.
+# Utilice las funciones min(), mean(), max(), de ser necesario utilice 
+# el parámetro na.rm = TRUE
+
+# 2.3 Para la variable Genero, contar cuantos sujetos son de Genero: Femenino.
+# Utilice la función table()
+
+# 2.4 Encontrar la Edad mínima, media, máxima de los sujetos que Si son dependientes.
+
+# 2.5 Identificar el tipo de elementos que contiene cada variable.
+# Utilice la función typeof()
+
+
+# 2.6 Identificar la clase de cada variable (columna).
+# Utilice la función class()
+
+# 2.7 Calcular la media de todas las variables numéricas (double, integer).
+# Recordar que para un factor no es posible obtener la media debido a que 
+# éstos representan variables
+
+# 2.8 Calcular el porcentaje de valores perdidos que contiene cada variable.
+# Utilice la función is.na()
+
+# 3. Selecionando sujetos mediante un determinado criterio:
+# 3.1 Seleccione los sujetos con una Edad mayor a 40 años.
+# Utilice la función subset()
+
+# 3.2 Seleccione los sujetos que tienen Vivienda Propia.
+
+# 3.3 Seleccione los sujetos que tienen más ($>$) de dos cargas familiatres.
+
+# 3.4 Seleccione los sujetos con una Deuda superior o igual a 500 dólares
+# y más ($>$) de 8 Dias_Atraso.
+
+# 3.5 Seleccione los sujetos con un Score mayor o igual a 900 puntos, una Edad menor
+# o igual a 35 años y con más ($>$) de 3 tarjetas de crédito (Numero_TC).
+
+# 4. Gráficos:
+# 4.1 Realice un histograma de la variable Edad, utilice como color de relleno: red
+
+# 4.2 Realice un diagrama de cajas de la variable Edad, utilice como color de relleno: green
+# Utilice la función boxplot()
+
+
